7f848193b6bc196d5593011f041ed9e38f2a5599
le.minh.hoang <le.minh.hoang@sun-asterisk.com>
2023-01-17 10:51:37 +0700

    chore: update circle ci config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,8 +2,6 @@ RSpec.configure do |config|
   config.before(type: :system) do |example|
     if example.metadata[:driver]
       driven_by example.metadata[:driver]
-    else
-      driven_by :selenium, using: :headless_chrome, screen_size: [1000, 800]
     end
   end
 end
